db2e0e08db205d29a8390d09f7dbad085832b334
Emanuele Fianco <emanuele.fianco@gmail.com>
2016-02-16 10:46:39 +0100

    Finite le condizioni complementari di Benia

---
+++ Code/Domain/Creator/CodizioneCreator/BombeRimanentiCondizioneCreator.java
@@ -0,0 +1,39 @@
+package Domain.Creator.CodizioneCreator;
+
+import Domain.Condizioni.BombeRimanentiCondizione;
+import Domain.Condizioni.ICondizione;
+
+import java.util.ArrayList;
+
+/**
+ * Created by emanuele on 16/02/16.
+ */
+public class BombeRimanentiCondizioneCreator implements ICondizioneCreator {
+
+    private static BombeRimanentiCondizioneCreator singletonInstance = null;
+
+    private String name;
+    private String description;
+    private String idType;
+
+    private BombeRimanentiCondizioneCreator() {
+        this.name = "Ho bombe rimanenti";
+        this.description = "Questa condizione controlla se il Tank ha ancora delle bombe";
+        this.idType = "010";
+    }
+
+    public static BombeRimanentiCondizioneCreator getSingletonInstance()
+    {
+        if (singletonInstance == null)
+        {
+            singletonInstance = new BombeRimanentiCondizioneCreator();
+        }
+        return singletonInstance;
+    }
+
+    @Override
+    public ICondizione doMakeCondizione(String idCond, ArrayList<Integer> valori) {
+        BombeRimanentiCondizione bombeRimanentiCondizione = new BombeRimanentiCondizione(this.name,this.description,this.idType,idCond,valori);
+        return bombeRimanentiCondizione;
+    }
+}
