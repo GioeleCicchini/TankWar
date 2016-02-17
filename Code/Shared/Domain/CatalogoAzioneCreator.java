c994235e857d39e9937cc005f23f42bbf211053e
Emanuele Fianco <emanuele.fianco@gmail.com>
2016-02-17 17:08:06 +0100

    Prendi benia

--- Code/Shared/Domain/CatalogoAzioneCreator.java
+++ Code/Shared/Domain/CatalogoAzioneCreator.java
@@ -1,23 +1,35 @@
 package Shared.Domain;
 
+import Shared.Domain.Creator.AzioneCreator.IAzioneCreator;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CatalogoAzioneCreator {
 
-	/**
-	 * 
-	 * @param idTypeAz
-	 */
-	public void getAzioneCreator(String idTypeAz) {
-		// TODO - implement CatalogoAzioneCreator.getAzioneCreator
-		throw new UnsupportedOperationException();
-	}
+	public static CatalogoAzioneCreator singletonInstance = null;
+
+    private Map<String,IAzioneCreator> azioniCreators;
+
+    private CatalogoAzioneCreator() {
+        this.azioniCreators=new HashMap<>();
+    }
+
+    public static CatalogoAzioneCreator getSingletonInstance()
+    {
+        if (singletonInstance == null)
+        {
+            singletonInstance= new CatalogoAzioneCreator();
+        }
+        return singletonInstance;
+    }
 
 	/**
 	 * 
 	 * @param idTypeAz
 	 */
-	public void getAzioneCreatore(int idTypeAz) {
-		// TODO - implement CatalogoAzioneCreator.getAzioneCreatore
-		throw new UnsupportedOperationException();
+	public IAzioneCreator getAzioneCreator(String idTypeAz) {
+        return this.azioniCreators.get(idTypeAz);
 	}
 
 }
