20f77852bbcf4a623d92c8cdbb914db6cbea6569
Beniamino Negrini <beniamino.negrini@gmail.com>
2016-02-22 12:31:19 +0100

    sta qui me sta a piace

--- Code/Client/UI/JavaFX/View/CreareStrategia.java
+++ Code/Client/UI/JavaFX/View/CreareStrategia.java
@@ -1,4 +1,15 @@
 package Client.UI.JavaFX.View;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class CreareStrategia {
+    public Label salutoLabel;
+    public Button faisalutoButton;
+
+
+    public void faiSaluto(ActionEvent actionEvent) {
+        salutoLabel.setText("Salutam a Soreta!");
+    }
 }
