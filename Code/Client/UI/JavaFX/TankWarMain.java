0af33076c5ebb6f3b80d57ce9400c18c4a874d3c
Beniamino Negrini <beniamino.negrini@gmail.com>
2016-02-24 12:16:25 +0100

    nuovo fxml bello bello

--- Code/Client/UI/JavaFX/TankWarMain.java
+++ Code/Client/UI/JavaFX/TankWarMain.java
@@ -1,25 +1,16 @@
 package Client.UI.JavaFX;
 
-import Client.UI.JavaFX.View.CreareStrategia;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import javax.swing.text.Document;
 
 public class TankWarMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Fxml/crearestartegia.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Fxml/crearestaT.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.setFullScreen(true);
