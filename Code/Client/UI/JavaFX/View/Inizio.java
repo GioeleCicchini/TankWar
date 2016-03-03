522805f98ca748556bb2a66e475cacbe4cabee95
Beniamino Negrini <beniamino.negrini@gmail.com>
2016-03-03 12:19:53 +0100

    push

---
+++ Code/Client/UI/JavaFX/View/Inizio.java
@@ -0,0 +1,21 @@
+package Client.UI.JavaFX.View;
+
+import javafx.event.Event;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by beniamino on 03/03/16.
+ */
+public class Inizio implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void startDemo(Event event) {
+        
+    }
+}
