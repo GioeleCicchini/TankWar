3455f424e33ca26bbbd44ca08f407865b7d4ef92
Emanuele Fianco <emanuele.fianco@gmail.com>
2016-03-04 10:38:30 +0100

    Ciao

--- Code/Client/UI/JavaFX/CustomWidget/ConditionLabel.java
+++ Code/Client/UI/JavaFX/CustomWidget/ConditionLabel.java
@@ -13,10 +13,14 @@ public class ConditionLabel extends Label implements ICustomLabel {
     private boolean vera;
 
     public ConditionLabel(String name, String description, String idCond, boolean vera) {
-        this.name = name;
+        this.vera = vera;
+        if (this.vera) {
+            this.name = "SE NON" + name;
+        } else {
+            this.name = name;
+        }
         this.description = description;
         this.idCond = idCond;
-        this.vera = vera;
     }
 
 
