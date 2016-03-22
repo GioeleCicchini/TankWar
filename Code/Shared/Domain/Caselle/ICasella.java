1674b590d0e7c41687bc7bc8fcfc80a1ef8a87b5
GioeleCicchini <gioele.cicchini@gmail.com>
2016-03-22 10:06:31 +0100

    Creazione Classi Seconda iterazione

---
+++ Code/Shared/Domain/Caselle/ICasella.java
@@ -0,0 +1,7 @@
+package Shared.Domain.Caselle;
+
+/**
+ * Created by gioele on 22/03/16.
+ */
+public interface ICasella {
+}
---
+++ Code/Shared/Domain/Caselle/MuroCasella.java
@@ -0,0 +1,10 @@
+package Shared.Domain.Caselle;
+
+
+import Shared.Domain.Caselle.ICasella;
+
+/**
+ * Created by gioele on 22/03/16.
+ */
+public class MuroCasella implements ICasella {
+}
