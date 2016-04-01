a5992ceb4c4dc69b325c2b512f45ef86c7612625
Emanuele Fianco <emanuele.fianco@gmail.com>
2016-04-01 17:27:04 +0200

    Fatte un po di condizioni

--- Code/Shared/Domain/TankDecorator/TankOnBattle.java
+++ Code/Shared/Domain/TankDecorator/TankOnBattle.java
@@ -4,6 +4,7 @@ import Shared.Domain.Caselle.ICasella;
 import Shared.Domain.ITank;
 import Shared.Domain.Strategia;
 import Shared.Domain.Tank;
+import Shared.Util.OrientamentoEnum;
 
 /**
  * Created by beniamino on 22/03/16.
@@ -11,9 +12,12 @@ import Shared.Domain.Tank;
 public class TankOnBattle extends TankDecorator {
 
     private ICasella casellaPosizione;
+    private Integer bombeRimanenti;
+    private OrientamentoEnum orientamento;
 
     public TankOnBattle(ITank component) {
         super(component);
+        this.bombeRimanenti = 10;
     }
 
     @Override
@@ -56,4 +60,37 @@ public class TankOnBattle extends TankDecorator {
         this.casellaPosizione = casellaPosizione;
     }
 
+    @Override
+    public Integer getBombeRimanenti() {
+        return bombeRimanenti;
+    }
+
+    @Override
+    public void setBombeRimanenti(Integer bombeRimanenti) {
+        this.bombeRimanenti = bombeRimanenti;
+    }
+
+    @Override
+    public OrientamentoEnum getOrientamento() {
+        return orientamento;
+    }
+
+    @Override
+    public void setOrientamento(OrientamentoEnum orientamento) {
+        this.orientamento = orientamento;
+    }
+
+    @Override
+    public void setOrientamento(int orientamento) {
+        switch (orientamento) {
+            case 0: this.orientamento = OrientamentoEnum.N;
+                break;
+            case 1: this.orientamento = OrientamentoEnum.E;
+                break;
+            case 2: this.orientamento = OrientamentoEnum.S;
+                break;
+            case 3: this.orientamento = OrientamentoEnum.O;
+                break;
+        }
+    }
 }
