72a6a093784e6b4df69ef7123ad562f818a05ae2
GioeleCicchini <gioele.cicchini@gmail.com>
2016-05-02 17:02:45 +0200

    Possibilità di selezionare con quale strategia si combatte

--- Code/Client/UI/JavaFX/View/ImpostareBattaglia.java
+++ Code/Client/UI/JavaFX/View/ImpostareBattaglia.java
@@ -2,9 +2,15 @@ package Client.UI.JavaFX.View;
 
 import Client.UI.UIUtils.ViewTransaction;
 import Shared.Controllers.SimulareBattagliaHandler;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -18,6 +24,7 @@ import java.util.ResourceBundle;
  */
 public class ImpostareBattaglia implements Initializable {
     public VBox StrategieSpace;
+    public Label StrategiaSelezionata;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -52,6 +59,34 @@ public class ImpostareBattaglia implements Initializable {
 
         List<Map> strategias = SimulareBattagliaHandler.getSingletonInstance().getStrategieList();
 
+        for(Map strategia : strategias){
+            boolean premuto = false;
+            Button nomeStrategia = new Button((String) strategia.get("nome"));
+            nomeStrategia.setPrefHeight(76.0);
+            nomeStrategia.setPrefWidth(313.0);
+            nomeStrategia.setStyle("-fx-background-color: #F63C1A;-fx-border-width:2px;-fx-border-color:black;");
+            StrategieSpace.setMargin(nomeStrategia,new Insets(10,0,0,20));
+            nomeStrategia.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if(premuto == false) {
+                        for(Node ButtonLista : StrategieSpace.getChildren()){
+                            ButtonLista.setStyle("-fx-background-color: #F63C1A;-fx-border-width:2px;-fx-border-color:black;");
+                        }
+                        StrategiaSelezionata.setText((String) strategia.get("nome"));
+                        nomeStrategia.setStyle("-fx-background-color: #1AF63C;-fx-border-width:2px;-fx-border-color:black;");
+                        SimulareBattagliaHandler.getSingletonInstance().scegliStrategia((String) strategia.get("id"));
+
+
+
+                    }
+
+
+                }
+            });
+
+            StrategieSpace.getChildren().add(nomeStrategia);
+        }
 
         System.out.println("qui");
     }
