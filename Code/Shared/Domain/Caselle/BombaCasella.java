9b00673e55e51e3c5d40625c8b8273e52db97506
Emanuele Fianco <emanuele.fianco@gmail.com>
2016-05-05 12:08:08 +0200

    Aggiustato il tank

--- Code/Shared/Domain/Caselle/BombaCasella.java
+++ Code/Shared/Domain/Caselle/BombaCasella.java
@@ -3,56 +3,19 @@ package Shared.Domain.Caselle;
 import Shared.Domain.ITank;
 import Shared.Domain.Posizione;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by beniamino on 31/03/16.
+ * Created by emanuele on 05/05/16.
  */
-public class PlainCasella implements ICasella,Serializable {
+public class BombaCasella implements ICasella {
 
     private boolean disponibile = true;
     private Posizione posizione;
     private String id;
-    private ITank tankOccupanteCasella = null;
-    private ITank bombaTank = null;
-
-
-
-    public PlainCasella(){}
-    public PlainCasella(Posizione posizione) {
-        this.posizione = posizione;
-    }
-
-    public ITank getTank() {
-        return tankOccupanteCasella;
-    }
-
-    public void setTank(ITank tank) {
-        this.tankOccupanteCasella = tank;
-        this.disponibile=false;
-    }
-
-    public void togliTank(){
-        tankOccupanteCasella=null;
-        disponibile=true;
-    }
-
-    @Override
-    public ITank getBombaTank() {
-        return bombaTank;
-    }
-
-    @Override
-    public void setBombaTank(ITank bombaTank) {
-        this.bombaTank = bombaTank;
-    }
-
-    @Override
-    public void togliBombaTank() {
-        this.bombaTank = null;
-    }
+    public ITank tankOccupanteCasella = null;
+    public ITank bombaTank = null;
 
     @Override
     public String getId() {
@@ -61,12 +24,49 @@ public class PlainCasella implements ICasella,Serializable {
 
     @Override
     public void setId(String id) {
-        this.id= id;
+        this.id = id;
     }
 
     @Override
     public boolean isDisponibile() {
         return disponibile;
+    }
+
+    @Override
+    public ITank getTank() {
+        return this.tankOccupanteCasella;
+    }
+
+    @Override
+    public void setTank(ITank tank) {
+        tankOccupanteCasella = tank;
+        this.disponibile = false;
+    }
+
+    @Override
+    public ITank getBombaTank() {
+        return bombaTank;
+    }
+
+    @Override
+    public void setBombaTank(ITank tank) {
+        this.bombaTank = tank;
+    }
+
+    @Override
+    public void togliBombaTank() {
+        this.bombaTank = null;
+    }
+
+    @Override
+    public void togliTank() {
+        this.tankOccupanteCasella = null;
+        this.disponibile = true;
+    }
+
+    @Override
+    public void setPosizione(Posizione posizione) {
+        this.posizione = posizione;
     }
 
     @Override
@@ -77,11 +77,10 @@ public class PlainCasella implements ICasella,Serializable {
     @Override
     public Map getMap() {
         Map casella = new HashMap();
-
         casella.put("Id",this.id);
         casella.put("Posizione",this.posizione.getMap());
         casella.put("Disponibile",this.disponibile);
-        casella.put("Tipo","PlainCasella");
+        casella.put("Tipo","BombaCasella");
         if(this.tankOccupanteCasella != null) {
             casella.put("tankOccupanteCasella", this.tankOccupanteCasella.getId());
         }
@@ -95,10 +94,5 @@ public class PlainCasella implements ICasella,Serializable {
             casella.put("bombaTank", null);
         }
         return casella;
-    }
-
-    @Override
-    public void setPosizione(Posizione posizione) {
-        this.posizione = posizione;
     }
 }
