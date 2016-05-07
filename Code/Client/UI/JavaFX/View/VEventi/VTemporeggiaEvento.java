47b8d94282a4fc74f96cbf84d64a3f756550050e
GioeleCicchini <gioele.cicchini@gmail.com>
2016-05-07 02:07:56 +0200

    Temporeggia Evento

---
+++ Code/Client/UI/JavaFX/View/VEventi/VTemporeggiaEvento.java
@@ -0,0 +1,32 @@
+package Client.UI.JavaFX.View.VEventi;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Gioele on 07/05/2016.
+ */
+public class VTemporeggiaEvento implements VEvento {
+    @Override
+    public void eseguiti(Map Evento, List<Label> Player) throws InterruptedException {
+        
+    }
+
+    @Override
+    public void setParametriGui(List<Node> Parametri) {
+
+    }
+
+    @Override
+    public void setTankOnBattleGui(Map tankOnBattleGui) {
+
+    }
+
+    @Override
+    public void setDimesioneCampo(Map dimesioneCampo) {
+
+    }
+}
